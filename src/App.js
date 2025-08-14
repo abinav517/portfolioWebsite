a7737a4088e83dd359aafdb24b0081149a9ec047
kiztbz <pragyapravita@gmail.com>
2025-08-15 01:07:23 +0530

    Adding links to pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
+
 function App() {
   return (
-    <div className="App">
-    <Header>
-    </Header>
-
-    <Footer>
-        
-    </Footer>
-    </div>
+     <Router>
+    <Header />
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
+      </Routes>
+    {/* <Footer /> */}
+    </Router>
   );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,18 @@
 import './header.css'
+
+
 export default function Header() {
-return(<div>
-    Header test
-    <ul className='mainheader'>
-      <li>Home</li>
-      <li>Projects</li>
-      <li>Contact</li>
-    </ul>
-  
-</div>)
+return(
+<div className='main-header'>
+  <div className='Logo'><a href='/'><img src="..\images\favicon 1.png"></img></a></div>
+  <div className='navLinks'>
+    <a href="/about">About</a>
+    <a href="#">Projects</a>
+    <a href="#">Show Reel</a>
+  </div>
+  <div className='cta'>
+    <a href="#">Contact</a>
+  </div>
+</div>
+)
 }
